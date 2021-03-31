c37b789bc916ab88b6ee7277622ac120e0751079
darshakpranpariya <darshak.ranpariya@gmail.com>
2021-03-31 17:14:18 +0530

    - HomeScreen Changes

--- lib/components/screens/dashboard/homescreen.dart
+++ lib/components/screens/dashboard/homescreen.dart
@@ -66,6 +66,7 @@ class _HomeScreenState extends State<HomeScreen> {
       isProgress: true,
     )
         .then((homeScreenResponse) async {
+          cateName.clear();
       cateName.addAll(homeScreenResponse.data.list);
       print("data scusedddd");
 
